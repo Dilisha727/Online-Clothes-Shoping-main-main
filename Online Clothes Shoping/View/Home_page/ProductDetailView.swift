d36795c2af2bcc76c35ca229216d8af49a6cddec
dilisha priyashan <dilishapriyashan076@gmail.com>
2024-03-26 13:11:33 +0530

    desing prodectView

---
+++ Online Clothes Shoping/View/Home_page/ProductDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetailView.swift
+//  Online Clothes Shoping
+//
+//  Created by NIBM-LAB04-PC04 on 2024-03-26.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductDetailView()
+}
